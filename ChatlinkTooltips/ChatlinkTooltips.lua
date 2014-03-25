5624b4ca90f2e8f3e721beb0f372a75f48857078
morrxy <morrxy@gmail.com>
2014-03-25 17:36:22 +0800

    add

---
+++ ChatlinkTooltips/ChatlinkTooltips.lua
@@ -0,0 +1,35 @@
+local function showTooltip(self, linkData)
+  local linkType = string.split(":", linkData)
+  if linkType == "item"
+  or linkType == "spell"
+  or linkType == "enchant"
+  or linkType == "quest"
+  or linkType == "talent"
+  or linkType == "glyph"
+  or linkType == "unit"
+  or linkType == "achievement" then
+    GameTooltip:SetOwner(self, "ANCHOR_CURSOR")
+    GameTooltip:SetHyperlink(linkData)
+    GameTooltip:Show()
+  end
+end
+
+local function hideTooltip()
+  GameTooltip:Hide()
+end
+
+local function setOrHookHandler(frame, script, func)
+  if frame:GetScript(script) then
+    frame:HookScript(script, func)
+  else
+    frame:SetScript(script, func)
+  end
+end
+
+for i = 1, NUM_CHAT_WINDOWS do
+  local frame = getglobal("ChatFrame" .. i)
+  if frame then
+    setOrHookHandler(frame, "OnHyperLinkEnter", showTooltip)
+    setOrHookHandler(frame, "OnHyperLinkLeave", hideTooltip)
+  end
+end
