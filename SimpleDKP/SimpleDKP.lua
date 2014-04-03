ebb98bbb128c622e55b0502b5f4a04a57a0f4be1
morrxy <morrxy@gmail.com>
2014-04-04 07:53:04 +0800

    m

--- SimpleDKP/SimpleDKP.lua
+++ SimpleDKP/SimpleDKP.lua
@@ -47,5 +47,42 @@ do
 
 end
 
+do
+
+  local noBids = prefix .. "No one wants to have %s :("
+  local wonItemFor = prefix .. "%s won %s for %d DKP"
+  local pleaseRoll = prefix .. "%s bid %d DKP on %s, please roll!"
+  local highestBidders = prefix .. "%d. %s bid %d DKP"
+
+  local function sortBids(v1, v2)
+    return v1.bid > v2.bid
+  end
+
+  function endAuction()
+    table.sort(bids, sortBids)
+    if #bids == 0 then -- case 1: no bid at all
+      SendChatMessage(noBids:format(currentItem), SimpleDKP_Channel)
+    elseif #bids == 1 then -- case 2: one bid; the bidder pays the minimun bid
+      SendChatMessage(wonItemFor:format(bids[1].name, currentItem, SimpleDKP_MinBid), SimpleDKP_Channel)
+      SendChatMessage(highestBidders:format(1, bids[1].name, bids[1].bid), SimpleDKP_Channel)
+    elseif bids[1].bid ~= bids[2].bid then -- case 3: highest amount is unique
+      SendChatMessage(wonItemFor:format(bids[1].name, currentItem, bids[2].bid + 1), SimpleDKP_Channel)
+      for i = 1, math.min(#bids, 3) do -- print the three highest bidders
+        SendChatMessage(highestBidders:format(i, bids[i].name, bids[i].bid), SimpleDKP_Channel)
+      end
+    else -- case 4: more than 1 bid and the highest amount is no unique
+      local str = "" -- this string holds all players who bid the same amount
+      for i = 1, #bids do -- this loop builds the string
+        if bids[i].bid ~= bids[1].bid then -- found a player who bid less --> break
+          break
+        else -- append the players name to the string
+
+        end
+      end
+  end
+
+
+end
+
 
 
